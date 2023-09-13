2314baa6995ffbf8fad3739b7c54c0e6c6c66e8e
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-13 11:21:24 +0200

    start functions of hypotheek

---
+++ Classes/Hypotheek.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypo.Classes
+{
+    public class Hypotheek
+    {
+        private int choice;
+        public void Maandinkomen()
+        {
+            Console.Write("Wat is je maandinkomen: ");
+            decimal BrutoJaarInkomen = decimal.Parse(Console.ReadLine());
+        }
+
+        public void RentevastePeriode()
+        {
+            while (true)
+            {
+                Console.WriteLine("Rentevaste periodes:");
+                Console.WriteLine("1. 1 jaar  | 2%");
+                Console.WriteLine("2. 5 jaar  | 3%");
+                Console.WriteLine("3. 10 jaar | 3.5%");
+                Console.WriteLine("4. 20 jaar | 4.5%");
+                Console.WriteLine("5. 30 jaar | 5%");
+
+                Console.Write("selecteer welke rentevaste periode u wilt kiezen voor uw hypotheek lening: ");
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("U heeft de rentevaste periode gekozen van: 1 jaar | 2%");
+                        break;
+                    case 2:
+                        Console.WriteLine("U heeft de rentevaste periode gekozen van: 5 jaar | 3%");
+                        break;
+                    case 3:
+                        Console.WriteLine("U heeft de rentevaste periode gekozen van: 10 jaar | 3.5%");
+                        break;
+                    case 4:
+                        Console.WriteLine("U heeft de rentevaste periode gekozen van: 20 jaar | 4.5%");
+                        break;
+                    case 5:
+                        Console.WriteLine("U heeft de rentevaste periode gekozen van: 30 jaar | 5%");
+                        break;
+                }
+            }
+        }
+
+        public void HeeftPartner()
+        {
+            Console.Write("Heeft u een partner (Ja/Nee): ");
+            string partnerInput = Console.ReadLine();
+            bool gebruikerHeeftPartner = partnerInput.Equals("Ja", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,19 +8,18 @@ namespace Hypo
     {
         static void Main(string[] args)
         {
-            Console.Write("Wat is je maandinkomen: ");
-            decimal maandloon = decimal.Parse(Console.ReadLine());
+            //create instance of hypotheek.
+            Hypo.Classes.Hypotheek hypotheek = new Hypo.Classes.Hypotheek();
 
-            Console.Write("Heb je een partner (Ja/Nee): ");
-            string partnerInput = Console.ReadLine();
-            bool heeftPartner = partnerInput.Equals("Ja", StringComparison.OrdinalIgnoreCase);
+            hypotheek.Maandinkomen();
+
+            hypotheek.RentevastePeriode();
+
+            hypotheek.HeeftPartner();
 
             Console.Write("Heb je een studieschuld (Ja/Nee): ");
             string studieschuldInput = Console.ReadLine();
             bool heeftStudieschuld = studieschuldInput.Equals("Ja", StringComparison.OrdinalIgnoreCase);
-
-            Console.Write("Voer de rentevaste periode in (1, 5, 10, 20, of 30 jaar): ");
-            int rentevastePeriode = int.Parse(Console.ReadLine());
 
             Console.Write("Voer je postcode in: ");
             string postcode = Console.ReadLine();
